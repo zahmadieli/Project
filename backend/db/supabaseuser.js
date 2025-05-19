90f9766daaa8618eef3e63db3a75706fb7a49c48
zahmadieli <zahmadi@terpmail.umd.edu>
2025-05-18 20:38:43 -0400

    fix supabase

--- backend/db/supabaseuser.js
+++ backend/db/supabaseuser.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://uqegiiwznkrnkgwzmnoo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZWdpaXd6bmtybmtnd3ptbm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTk3OTMsImV4cCI6MjA2MzE3NTc5M30.-XC1ivuJ17SUMeSjVXHHiO1MybFx2-SjiNqgQg3XeTA';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
