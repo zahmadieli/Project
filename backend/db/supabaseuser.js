26ce114db04b48503ba6cdb6f08628c723fea79d
zahmadieli <zahmadi@terpmail.umd.edu>
2025-05-18 20:50:35 -0400

    Connect to new Supabase project

--- backend/db/supabaseuser.js
+++ backend/db/supabaseuser.js
@@ -1,7 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://uqegiiwznkrnkgwzmnoo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZWdpaXd6bmtybmtnd3ptbm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTk3OTMsImV4cCI6MjA2MzE3NTc5M30.-XC1ivuJ17SUMeSjVXHHiO1MybFx2-SjiNqgQg3XeTA';
-
+const supabaseUrl = 'https://bsavohwgvaoexodeugxh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzYXZvaHdndmFvZXhvZGV1Z3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MTM5OTMsImV4cCI6MjA2MzE4OTk5M30.ddts1HzjDSlnveJjFyWe6iWAuSH21f1XZQRpWBVSyV8'
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
